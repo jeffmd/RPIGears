64ced9181bf954744479a7e254b970d4ad7c4498
jeffmd <shelleydoyle@eastlink.ca>
2018-11-09 14:51:58 -0400

    flesh out frame buffer support - still a lot missing - color attachments are textures - depth and stencil attachments are always render buffers

--- gpu_framebuffer.c
+++ gpu_framebuffer.c
@@ -1,8 +1,11 @@
 // gpu_framebuffer.c
 
 #include <stdio.h>
+#include <stdint.h>
 
 #include "gles3.h"
+
+#define GPU_FRAMEBUFFER_MAX_COUNT 5
 
 typedef enum {
 	GPU_FB_DEPTH_ATTACHMENT = 0,
@@ -20,10 +23,36 @@ typedef struct {
 typedef struct {
 	//GPUContext *ctx;
 	uint16_t object;
+	uint8_t refcount;
 	//uint16_t dirty_flag;
 	int width, height;
 	unsigned type:2;
-	uint16_t color_buffer;
-    uint16_t depth_renderBuffer;
-    uint16_t stencil_renderBuffer; 
+	uint16_t attachments[GPU_FB_MAX_ATTACHEMENT];
 } GPUFrameBuffer;
+
+static GPUFrameBuffer framebuffers[GPU_FRAMEBUFFER_MAX_COUNT];
+static uint16_t next_deleted_framebuffer = 0;
+
+static GLuint find_deleted_framebuffer(void)
+{
+  
+	for (GLuint id = next_deleted_framebuffer; id < GPU_FRAMEBUFFER_MAX_COUNT; id++) {
+	  if (framebuffers[id].refcount == 0) {
+		  next_deleted_framebuffer = id + 1;
+	    return id;
+	  }
+	}
+  
+    printf("WARNING: No Frame Buffers available\n");
+	return GPU_FRAMEBUFFER_MAX_COUNT - 1;
+}
+
+static GLenum convert_attachment_type_to_gl(GPUAttachmentType type)
+{
+	static const GLenum table[] = {
+		[GPU_FB_COLOR_ATTACHMENT0] = GL_COLOR_ATTACHMENT0,
+		[GPU_FB_STENCIL_ATTACHMENT] = GL_STENCIL_ATTACHMENT,
+		[GPU_FB_DEPTH_ATTACHMENT] = GL_DEPTH_ATTACHMENT
+	};
+	return table[type];
+}
