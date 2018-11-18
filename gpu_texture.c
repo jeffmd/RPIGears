489f87cb890293087c6da8916e3be96c03eb90bb
jeffmd <shelleydoyle@eastlink.ca>
2018-11-18 01:59:53 -0400

    gpu_texture: id 0 is the default - don't delete gl texture id, just re-use it - fix memory size calculation

--- gpu_texture.c
+++ gpu_texture.c
@@ -40,10 +40,10 @@ static GLenum gpu_get_gl_dataformat(GPUTextureFormat data_type)
     case GPU_RG8: return GL_LUMINANCE_ALPHA; break;
     case GPU_RGB8: return GL_RGB; break;
     case GPU_RGBA8: return GL_RGBA; break;
+    case GPU_STENCIL8: return GL_STENCIL_INDEX8; break;
     case GPU_DEPTH16: return GL_DEPTH_COMPONENT16; break;
     case GPU_DEPTH24: return GL_DEPTH_COMPONENT24_OES; break;
     case GPU_DEPTH32: return GL_DEPTH_COMPONENT32_OES; break;
-    case GPU_STENCIL8: return GL_STENCIL_INDEX8; break;
     default:
       return GL_RGBA;
   }
@@ -59,8 +59,8 @@ static GLuint gpu_texture_memory_footprint_compute(const GLuint texID)
 {
 
   GPUTexture *tex = &textures[texID];
-  
-  GLuint size = tex->format * tex->width * tex->height;
+  const GLuint bytes = (tex->format < GPU_STENCIL8) ? tex->format : tex->format - GPU_RGBA8;
+  GLuint size = bytes * tex->width * tex->height;
   if (tex->target == GL_TEXTURE_CUBE_MAP) size *= 6; 
   
   return size;
@@ -79,6 +79,7 @@ static void gpu_texture_memory_footprint_remove(const GLuint texID)
 
 static GLuint find_deleted_texture(void)
 {
+  if(next_deleted_texture == 0) next_deleted_texture = 1;
   
 	for (GLuint id = next_deleted_texture; id < GPU_TEXTURE_MAX_COUNT; id++) {
 	  if (textures[id].refcount == 0) {
@@ -88,26 +89,26 @@ static GLuint find_deleted_texture(void)
 	}
   
   printf("WARNING: No Textures available\n");
-	return GPU_TEXTURE_MAX_COUNT - 1;
+	return GPU_TEXTURE_MAX_COUNT;
 }
 
-static void init_renderbuffer( GPUTexture *tex)
+static void init_renderbuffer(GPUTexture *tex)
 {
   tex->is_texture = 0;
   tex->target = GL_RENDERBUFFER;
-  glGenRenderbuffers(1, &tex->bindcode);
+  if(!tex->bindcode) glGenRenderbuffers(1, &tex->bindcode);
   glBindRenderbuffer(GL_RENDERBUFFER, tex->bindcode);
   const GLenum data_format = gpu_get_gl_dataformat(tex->format);
   glRenderbufferStorage(GL_RENDERBUFFER, data_format, tex->width, tex->height);
   glBindRenderbuffer(GL_RENDERBUFFER, 0);
 }
 
-static void init_texture( GPUTexture *tex, const void *pixels)
+static void init_texture(GPUTexture *tex, const void *pixels)
 {
   tex->is_texture = 1;
   tex->target = GL_TEXTURE_2D;
   /* Generate Texture object */
-  glGenTextures(1, &tex->bindcode);//GPU_tex_alloc();
+  if(!tex->bindcode) glGenTextures(1, &tex->bindcode);//GPU_tex_alloc();
 
   //if (!tex->bindcode) {
     //printf("GPUTexture: texture create failed\n");
@@ -131,7 +132,7 @@ static void init_texture( GPUTexture *tex, const void *pixels)
 
 }
 
-GLuint GPU_texture_create_2D( const int w, const int h,
+GLuint GPU_texture_create_2D(const int w, const int h,
             const GPUTextureFormat tex_format, const void *pixels)
 {
   const GLuint texID = find_deleted_texture();
@@ -212,7 +213,7 @@ void GPU_texture_free(const GLuint texID)
 	if (tex->refcount == 0) {
 		GPU_framebuffer_texture_detach_all(texID);
     
-		glDeleteTextures(1, &tex->bindcode);//GPU_tex_free(tex->bindcode);
+		//glDeleteTextures(1, &tex->bindcode);//GPU_tex_free(tex->bindcode);
 
 		gpu_texture_memory_footprint_remove(texID);
 
