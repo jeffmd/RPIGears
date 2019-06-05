3d30facc8bc3d59528a6ae4fa57b0dade30f4352
jeffmd <shelleydoyle@eastlink.ca>
2019-06-05 01:23:50 -0300

    use vec2 to form vec4

--- shaders/test_quad_vert.glsl
+++ shaders/test_quad_vert.glsl
@@ -1,16 +1,16 @@
 #version 100
 // test_quad_vert.glsl
+
 attribute vec4 position_uv;
 
 varying vec2 oUV;
 
-#define SCALE 0.12
+#define SCALE 0.105
 
 void main(void)
 {
-    gl_Position = vec4(position_uv.x * SCALE, position_uv.y * SCALE, 0.0, 1.0);
+    gl_Position = vec4(position_uv.xy * SCALE, 0.0, 1.0);
 
     oUV = position_uv.zw;
-    // Transform the position to clip coordinates
 }
  
