4bf3259f816ce81eeeae5f136c5c2b31c9cc78ae
jeffmd <shelleydoyle@eastlink.ca>
2019-08-10 01:50:44 -0300

    static_array: remove unused functions

--- static_array.c
+++ static_array.c
@@ -2,29 +2,6 @@
 
 #include <stdio.h>
 
-void *array_find_deleted(void **next_deleted, void *start, const void *end, size_t size, const char *object_name)
-{
-  void *deleted = *next_deleted;
-
-  if ((deleted <= start) | (deleted >= end))
-    deleted = start + size;
-
-  for ( ; deleted < end; deleted += size) {
-    if (*(char *)deleted == 0) {
-      *next_deleted = deleted + size;
-      break;
-    }
-  }
-  
-  if (deleted >= end) {
-    printf("WARNING: No %s available\n", object_name);
-    deleted = start;
-  }
-  
-  printf("%s ID: %i\n", object_name, (deleted - start)  / size);
-  return deleted;
-}
-
 int array_find_deleted_id(int *next_deleted, void *array, const int maxId, const size_t size, const char *object_name)
 {
   int id = *next_deleted;
