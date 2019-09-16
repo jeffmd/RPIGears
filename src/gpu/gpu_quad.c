24c982ad7b8a2fcfd4bb0a324f2fd8cc12a0fdd9
jeffmd <shelleydoyle@eastlink.ca>
2019-09-16 19:25:25 -0300

    gpu_quad: use triangle strip to draw quad - 2 less vertices per quad and a little faster to render

--- src/gpu/gpu_quad.c
+++ src/gpu/gpu_quad.c
@@ -97,12 +97,12 @@ static void gpu_quad_batch_init(GPUQuad *quad)
   }
 
   const int vbuff = GPU_batch_vertex_buffer(quad->batch);
-  GPU_batch_set_vertices_draw_count(quad->batch, 6);
+  GPU_batch_set_vertices_draw_count(quad->batch, 4);
   GPU_vertbuf_set_vertex_format(vbuff, get_quad_vformat());
-  GPU_vertbuf_set_add_count(vbuff, 6);
+  GPU_vertbuf_set_add_count(vbuff, 4);
   
 #define VERTEX(x, y) (GPU_vertbuf_add_4(vbuff, ATTR_POSITION, x, y, ((x) + 1)*0.5, (-(y) + 1)*0.5))
-#define QUAD(x, y, dx, dy) VERTEX(x+dx, y); VERTEX(x, y+dy); VERTEX(x, y);  VERTEX(x+dx, y); VERTEX(x+dx, y+dy); VERTEX(x, y+dy);
+#define QUAD(x, y, dx, dy) VERTEX(x, y); VERTEX(x+dx, y); VERTEX(x, y+dy); VERTEX(x+dx, y+dy);
   
   QUAD(-1, -1, 2, 2)
 }
@@ -125,7 +125,7 @@ void GPU_quad_draw(const int id)
   GPU_texture_bind(quad->texture, 0);
   GPU_shader_bind(quad->shader);;
   GPU_uniformbuffer_activate(0);
-  GPU_batch_draw(quad->batch, GL_TRIANGLES, 1);  
+  GPU_batch_draw(quad->batch, GL_TRIANGLE_STRIP, 1);  
 }
 
 void GPU_quad_set_texture(const int id, const int tex_id)
