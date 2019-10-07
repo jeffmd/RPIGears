497fd7492faf72ce0d1397ba4f963eacb1b73dda
jeffmd <shelleydoyle@eastlink.ca>
2019-10-07 18:39:11 -0300

    gpu_uniformbuffer: fixed error in GPU_uniformbuffer_add_1f macro

--- include/gpu/gpu_uniform_buffer.h
+++ include/gpu/gpu_uniform_buffer.h
@@ -14,7 +14,7 @@
   void GPU_uniformbuffer_activate(const int id);
 
 #define GPU_uniformbuffer_add_4f(ubuff, name, data) GPU_uniformbuffer_add(ubuff, name, sizeof(data)/(sizeof(float)*4), GL_FLOAT_VEC4, &data)
-#define GPU_uniformbuffer_add_1f(ubuff, name, data) GPU_uniformbuffer_addm(ubuff, name, sizeof(data)/sizeof(float), GL_FLOAT, &data)
+#define GPU_uniformbuffer_add_1f(ubuff, name, data) GPU_uniformbuffer_add(ubuff, name, sizeof(data)/sizeof(float), GL_FLOAT, &data)
 #define GPU_uniformbuffer_add_1i(ubuff, name, data) GPU_uniformbuffer_add(ubuff, name, sizeof(data)/sizeof(int), GL_INT, &data)
 #define GPU_uniformbuffer_add_4x4m(ubuff, name, data) GPU_uniformbuffer_add(ubuff, name, sizeof(data)/(sizeof(float)*4*4), GL_FLOAT_MAT4, &data)
 
