b9cb7e35e003507ab615112f2c4068852a5d8f82
jeffmd <shelleydoyle@eastlink.ca>
2020-08-18 15:59:52 -0300

    connector: simplify action management code

--- src/ui/connector.c
+++ src/ui/connector.c
@@ -8,13 +8,9 @@
 typedef void (*ActionFn)(const short source_id, const short destination_id);
 
 typedef struct {
-  ActionFn action;
-} ActionSlot;
-
-typedef struct {
   uint8_t active;
   uint8_t count;
-  short action_slot;
+  short action_start;
   short source_class;
   short destination_class;
 } Connector;
@@ -33,10 +29,10 @@ typedef union {
 
 
 #define CONNECTOR_MAX_COUNT 50
-#define ACTION_SLOT_MAX_COUNT 1000
+#define ACTION_MAX_COUNT 1000
 
-static ActionSlot action_slots[ACTION_SLOT_MAX_COUNT];
-static short next_action_slot = 0;
+static ActionFn actions[ACTION_MAX_COUNT];
+static short next_action;
 
 static Connector connectors[CONNECTOR_MAX_COUNT];
 static short next_deleted_connector;
@@ -45,13 +41,13 @@ static short next_deleted_connector;
 static ClassReg classes[CLASSREG_MAX_COUNT];
 static short next_class_id;
 
-static short action_slot_allocate(const int count)
+static short actions_allocate(const int count)
 {
-  const short new_action_slot = next_action_slot;
+  const short action_start = next_action;
 
-  next_action_slot += count;
+  next_action += count;
 
-  return new_action_slot;
+  return action_start;
 }
  
 static inline short find_deleted_connector(void)
@@ -74,20 +70,20 @@ static void connector_init(Connector *connector)
   //action_table->action_slot = 0;
 }
 
-static void action_slots_init(const short id, const short count)
+static void actions_init(const short id, const short count)
 {
   const short last_id = id + count;
   for (short idx = id; idx < last_id; idx++) {
-    action_slots[idx].action = 0;
+    actions[idx] = 0;
   }
 }
 
-static void allocate_slots(Connector *const connector, const short count)
+static void allocate_actions(Connector *const connector, const short count)
 {
-  connector->action_slot = action_slot_allocate(count);
+  connector->action_start = actions_allocate(count);
   connector->count = count;
 
-  action_slots_init(connector->action_slot, count);
+  actions_init(connector->action_start, count);
 }
 
 
@@ -100,7 +96,7 @@ short Connector_create(const short source_class, const short destination_class, 
   connector->source_class = source_class;
   connector->destination_class = destination_class;
 
-  allocate_slots(connector, count);
+  allocate_actions(connector, count);
 
   return id;
 }
@@ -110,7 +106,7 @@ void Connector_set_action(const short connector_id, const short slot_id, ActionF
   Connector *const connector = get_connector(connector_id);
 
   if (slot_id < connector->count) {
-    action_slots[connector->action_slot + slot_id].action = action;
+    actions[connector->action_start + slot_id] = action;
   }
 }
 
@@ -138,9 +134,9 @@ void Connector_handle_execute(const int handle, const short slot_id, const short
     Connector *const connector = get_connector(((Handle)handle).connector);
 
     if (slot_id < connector->count) {
-      ActionSlot *slot = action_slots + connector->action_slot + slot_id;
-      if (slot->action) {
-        slot->action(source_id, ((Handle)handle).destination_id);
+      ActionFn action = actions[connector->action_start + slot_id];
+      if (action) {
+        action(source_id, ((Handle)handle).destination_id);
       }
     }
   }
