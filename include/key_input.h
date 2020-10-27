5f170e72c0aee4108925df695ebeb2e4880e5606
jeffmd <shelleydoyle@eastlink.ca>
2020-10-27 12:02:57 -0300

    key_input: BugFix - STATE_KEY needs surrounding brackets to ensure proper evaluation

--- include/key_input.h
+++ include/key_input.h
@@ -8,7 +8,7 @@
   #define SHIFTK 1<<0
   #define CTRLK  1<<2
   #define ALTK   1<<3
-  #define STATE_KEY(state, key) (state << 8) + key
+  #define STATE_KEY(state, key) ((state << 8) + key)
   #define SHIFT_KEY(key) STATE_KEY(SHIFTK, key)
   #define CTRL_KEY(key) STATE_KEY(CTRLK, key)
   #define SHIFT_CTRL_KEY(key) STATE_KEY((SHIFTK | CTRLK), key)
