9f8f988d3a608c0ceb0479d6a9d2df1dfef0f9c1
jeffmd <shelleydoyle@eastlink.ca>
2021-01-27 20:49:28 -0400

    window: broke large functions into smaller more manageable functions - shourldn't have to worry about order of initialization when creating new window

--- src/window.c
+++ src/window.c
@@ -17,13 +17,13 @@
 typedef  struct {
 // OpenGL|ES objects
    EGLConfig config;
-   EGLDisplay display;
+   EGLDisplay egl_display;
    EGLSurface surface;
    EGLContext contextGLES2;
 // EGL info
    int major;
    int minor;
-   EGL_DISPMANX_WINDOW_T nativewindow;
+   EGL_DISPMANX_WINDOW_T native_window;
    DISPMANX_DISPLAY_HANDLE_T dispman_display;
    VC_RECT_T dst_rect;
    VC_RECT_T src_rect;
@@ -49,12 +49,12 @@ int Window_minor(void)
 
 int Window_screen_width(void)
 {
-  return window->nativewindow.width;
+  return window->native_window.width;
 }
 
 int Window_screen_height(void)
 {
-  return window->nativewindow.height;
+  return window->native_window.height;
 }
 
 static void window_update_old(void)
@@ -65,13 +65,13 @@ static void window_update_old(void)
   window->old_rect.y = window->dst_rect.y;
 }
 
-static void updateSrcSize(void)
+static void update_src_size(void)
 {
   window->src_rect.width = (window->dst_rect.width) << 16;
   window->src_rect.height = (window->dst_rect.height) << 16;
-  window->offsetx = (window->nativewindow.width - window->dst_rect.width) / 2;
+  window->offsetx = (window->native_window.width - window->dst_rect.width) / 2;
   window->src_rect.x = window->offsetx << 16;
-  window->offsety = (window->nativewindow.height - window->dst_rect.height) / 2;
+  window->offsety = (window->native_window.height - window->dst_rect.height) / 2;
   window->src_rect.y = window->offsety << 16;
 }
 
@@ -81,7 +81,7 @@ static void window_update(void)
   assert(update != 0);
 
   int result = vc_dispmanx_element_change_attributes(update,
-                  window->nativewindow.element,
+                  window->native_window.element,
                   0/*ELEMENT_CHANGE_OPACITY | ELEMENT_CHANGE_TRANSFORM*/,
                   0,
                   0,
@@ -129,7 +129,7 @@ void Window_size(const int width, const int height)
   window->dst_rect.width = width;
   window->dst_rect.height = height;
   window_update_old();
-  updateSrcSize();
+  update_src_size();
   window_update();
 }
 
@@ -143,70 +143,71 @@ void Window_pos(const int x, const int y)
 
 void Window_update_VSync(const int sync)
 {
-  const EGLBoolean result = eglSwapInterval(window->display, sync );
+  const EGLBoolean result = eglSwapInterval(window->egl_display, sync );
   assert(egl_chk(EGL_FALSE != result));
 }
 
-static void createSurface(void)
+static void create_native_window(void)
 {
   DISPMANX_UPDATE_HANDLE_T dispman_update;
 
-  int32_t success = graphics_get_display_size(0 /* LCD */, (uint32_t *)&window->nativewindow.width, (uint32_t *)&window->nativewindow.height);
+  int32_t success = graphics_get_display_size(0 /* LCD */, (uint32_t *)&window->native_window.width, (uint32_t *)&window->native_window.height);
   assert( success >= 0 );
+
   window_size_reset();
+  update_src_size();
 
   window->dispman_display = vc_dispmanx_display_open( 0 /* LCD */);
 
-  dispman_update = vc_dispmanx_update_start( 0 );
-  
   VC_DISPMANX_ALPHA_T alphadata = {
     DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS | DISPMANX_FLAGS_ALPHA_MIX | DISPMANX_FLAGS_ALPHA_PREMULT,
     0,
     0
   };
   
-  window->nativewindow.element = vc_dispmanx_element_add( dispman_update, window->dispman_display,
+  printf("creating native window: dispmanx element\n");
+  dispman_update = vc_dispmanx_update_start( 0 );
+ 
+  window->native_window.element = vc_dispmanx_element_add( dispman_update, window->dispman_display,
     0/*layer*/, &window->dst_rect, 0/*src*/,
     &window->src_rect, DISPMANX_PROTECTION_NONE, &alphadata /*alpha*/, 0/*clamp*/, DISPMANX_ROTATE_90/*transform*/);
-
   vc_dispmanx_update_submit(dispman_update, 0, 0);
 
-#if 0
-  EGLint pixel_format = EGL_PIXEL_FORMAT_ARGB_8888_BRCM;
-
-  pixel_format |= EGL_PIXEL_FORMAT_RENDER_GLES2_BRCM;
-  pixel_format |= EGL_PIXEL_FORMAT_GLES2_TEXTURE_BRCM;
-
-  EGLint pixmap[5];
-        pixmap[0] = 0;
-        pixmap[1] = 0;
-        pixmap[2] = window->nativewindow.width;
-        pixmap[3] = window->nativewindow.height;
-        pixmap[4] = pixel_format;
-
-  EGLint attrs[] = {
-    EGL_VG_COLORSPACE, EGL_VG_COLORSPACE_sRGB,
-    EGL_VG_ALPHA_FORMAT, pixel_format & EGL_PIXEL_FORMAT_ARGB_8888_PRE_BRCM ? EGL_VG_ALPHA_FORMAT_PRE : EGL_VG_ALPHA_FORMAT_NONPRE,
-    EGL_NONE
-  };
-
-  eglCreateGlobalImageBRCM(window->nativewindow.width, window->nativewindow.height, pixmap[4], 0, window->nativewindow.width*4, pixmap);
-  window->surface = eglCreatePixmapSurface(window->display, window->config, (EGLNativePixmapType)pixmap, attrs);
-#else
-  /*EGLint attribw[5];
-        attribw[0] = EGL_HEIGHT;
-        attribw[1] = window->nativewindow.height;
-        attribw[2] = EGL_WIDTH;
-        attribw[3] = window->nativewindow.width;
-        attribw[4] = EGL_NONE;
-  window->surface = eglCreatePbufferSurface(window->display, window->config, attribw);
-  */
-  window->surface = eglCreateWindowSurface( window->display, window->config, &window->nativewindow, NULL );
-#endif
-  assert(egl_chk(window->surface != EGL_NO_SURFACE));
+  Window_hide();
 }
 
-static void createContext(void)
+static NativeWindowType get_native_window(void)
+{
+
+  if (!window->native_window.element) {
+    create_native_window();
+  }
+
+  return &window->native_window;
+}
+
+static void create_egl_display(void)
+{
+  // get an EGL display connection
+  printf("creating egl display\n");
+  window->egl_display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+  assert(egl_chk(window->egl_display!=EGL_NO_DISPLAY));
+
+  // initialize the EGL display connection
+  EGLBoolean result = eglInitialize(window->egl_display, &window->major, &window->minor);
+  assert(egl_chk(EGL_FALSE != result));
+}
+
+static EGLDisplay get_egl_display(void)
+{
+  if (!window->egl_display) {
+    create_egl_display();
+  }
+
+  return window->egl_display;
+}
+
+static void create_config(void)
 {
   EGLBoolean result;
   EGLint num_config;
@@ -224,57 +225,101 @@ static void createContext(void)
     EGL_NONE
   };
 
-  static EGLint context_attributes[] =
-  {
-    EGL_CONTEXT_CLIENT_VERSION, 2,
-    EGL_NONE
-  };
-
-
-  // get an EGL display connection
-  window->display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-  assert(egl_chk(window->display!=EGL_NO_DISPLAY));
-
-  // initialize the EGL display connection
-  result = eglInitialize(window->display, &window->major, &window->minor);
-  assert(egl_chk(EGL_FALSE != result));
-
   // get an appropriate EGL frame buffer configuration
-  result = eglChooseConfig(window->display, attribute_list, &window->config, 1, &num_config);
+  printf("creating EGL configuration\n");
+  result = eglChooseConfig(get_egl_display(), attribute_list, &window->config, 1, &num_config);
   assert(egl_chk(EGL_FALSE != result));
   printf("number of configs available: %i\n", num_config);
 
   // bind the gles api to this thread - this is default so not required
   result = eglBindAPI(EGL_OPENGL_ES_API);
   assert(egl_chk(EGL_FALSE != result));
+}
+
+static EGLConfig get_config(void)
+{
+  if (!window->config) {
+    create_config();
+  }
+
+  return window->config;
+}
+
+static void create_context(void)
+{
+  static EGLint context_attributes[] =
+  {
+    EGL_CONTEXT_CLIENT_VERSION, 2,
+    EGL_NONE
+  };
 
   // create an EGL rendering context
-
-  window->contextGLES2 = eglCreateContext(window->display, window->config, EGL_NO_CONTEXT, context_attributes);
+  printf("creating GLES 2 context\n");
+  window->contextGLES2 = eglCreateContext(get_egl_display(), get_config(), EGL_NO_CONTEXT, context_attributes);
   assert(egl_chk(window->contextGLES2 != EGL_NO_CONTEXT));
 }
 
+static EGLContext get_context(void)
+{
+  if (!window->contextGLES2) {
+    create_context();
+  }
 
+  return window->contextGLES2;
+}
 
-/***********************************************************
- * Name: init_window
- *
- *
- * Description: Sets the display, OpenGL|ES context and screen stuff
- *
- * Returns: void
- *
- ***********************************************************/
+static void create_surface(void)
+{
+
+#if 0
+  EGLint pixel_format = EGL_PIXEL_FORMAT_ARGB_8888_BRCM;
+
+  pixel_format |= EGL_PIXEL_FORMAT_RENDER_GLES2_BRCM;
+  pixel_format |= EGL_PIXEL_FORMAT_GLES2_TEXTURE_BRCM;
+
+  EGLint pixmap[5];
+        pixmap[0] = 0;
+        pixmap[1] = 0;
+        pixmap[2] = window->native_window.width;
+        pixmap[3] = window->native_window.height;
+        pixmap[4] = pixel_format;
+
+  EGLint attrs[] = {
+    EGL_VG_COLORSPACE, EGL_VG_COLORSPACE_sRGB,
+    EGL_VG_ALPHA_FORMAT, pixel_format & EGL_PIXEL_FORMAT_ARGB_8888_PRE_BRCM ? EGL_VG_ALPHA_FORMAT_PRE : EGL_VG_ALPHA_FORMAT_NONPRE,
+    EGL_NONE
+  };
+
+  eglCreateGlobalImageBRCM(window->native_window.width, window->native_window.height, pixmap[4], 0, window->native_window.width*4, pixmap);
+  window->surface = eglCreatePixmapSurface(window->display, window->config, (EGLNativePixmapType)pixmap, attrs);
+#else
+  /*EGLint attribw[5];
+        attribw[0] = EGL_HEIGHT;
+        attribw[1] = window->native_window.height;
+        attribw[2] = EGL_WIDTH;
+        attribw[3] = window->native_window.width;
+        attribw[4] = EGL_NONE;
+  window->surface = eglCreatePbufferSurface(window->display, window->config, attribw);
+  */
+  printf("creating EGL window surface\n");
+  window->surface = eglCreateWindowSurface( get_egl_display(), get_config(), get_native_window(), NULL );
+#endif
+  assert(egl_chk(window->surface != EGL_NO_SURFACE));
+}
+
+static EGLSurface get_surface(void)
+{
+  if (!window->surface) {
+    create_surface();
+  }
+
+  return window->surface;
+}
+
 void Window_init(void)
 {
-  EGLBoolean result;
-
-  createContext();
-  // create an EGL window surface based on current screen size
-  createSurface();
-  Window_hide();
   // connect the context to the surface
-  result = eglMakeCurrent(window->display, window->surface, window->surface, window->contextGLES2);
+  EGLBoolean result = eglMakeCurrent(get_egl_display(), get_surface(), get_surface(), get_context());
   assert(egl_chk(EGL_FALSE != result));
 
   // Set background color and clear buffers
@@ -282,6 +327,7 @@ void Window_init(void)
 
   // Enable back face culling.
   glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
   glFrontFace(GL_CCW);
   glEnable(GL_DEPTH_TEST);
   //glLineWidth(1.0);
@@ -297,14 +343,14 @@ void Window_init(void)
 
 void Window_swap_buffers(void)
 {
-  assert(egl_chk(eglSwapBuffers(window->display, window->surface)));
-  //assert(egl_chk(eglCopyBuffers(window->display, window->surface, &window->nativewindow)));
+  assert(egl_chk(eglSwapBuffers(window->egl_display, window->surface)));
+  //assert(egl_chk(eglCopyBuffers(window->display, window->surface, &window->native_window)));
 }
 
 void Window_viewport_reset(void)
 {
   glBindFramebuffer(GL_FRAMEBUFFER, 0);
-  glViewport(0, 0, window->nativewindow.width, window->nativewindow.height);
+  glViewport(0, 0, window->native_window.width, window->native_window.height);
   glDisable(GL_SCISSOR_TEST);
 }
 
@@ -313,30 +359,35 @@ void Window_ui_viewport(short pos[2], short size[2], short vis_pos[4])
   short y = window->old_rect.height - pos[1] - size[1];
   const int ox = pos[0] + (size[0] - window->old_rect.width ) / 2 ;
   const int oy = y + (size[1] - window->old_rect.height) / 2;
-  glViewport(ox, oy, window->nativewindow.width, window->nativewindow.height);
+  glViewport(ox, oy, window->native_window.width, window->native_window.height);
   y = window->old_rect.height - vis_pos[3];
   glScissor(window->offsetx + vis_pos[0], window->offsety + y, vis_pos[2] - vis_pos[0], vis_pos[3] - vis_pos[1]);
 }
 
 void Window_release(void)
 {
-  eglMakeCurrent( window->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT );
-  eglDestroySurface( window->display, window->surface );
-  eglDestroyContext( window->display, window->contextGLES2 );
-  eglTerminate( window->display );
+  eglMakeCurrent( window->egl_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT );
+  eglDestroySurface( window->egl_display, window->surface );
+  window->surface = 0;
+  eglDestroyContext( window->egl_display, window->contextGLES2 );
+  window->contextGLES2 = 0;
+  eglTerminate( window->egl_display );
+  window->egl_display = 0;
 
   DISPMANX_UPDATE_HANDLE_T dispman_update = vc_dispmanx_update_start(0);
-  vc_dispmanx_element_remove( dispman_update, window->nativewindow.element );
+  vc_dispmanx_element_remove( dispman_update, window->native_window.element );
+  window->native_window.element = 0;
   vc_dispmanx_update_submit_sync( dispman_update );
 
   vc_dispmanx_display_close(window->dispman_display);
+  window->dispman_display = 0;
 
 }
 
 void Window_snapshot(const int width, const int height, void * buffer)
 {
-  const int x = (window->nativewindow.width - width) / 2;
-  const int y = (window->nativewindow.height - height) / 2;
+  const int x = (window->native_window.width - width) / 2;
+  const int y = (window->native_window.height - height) / 2;
   glReadPixels(x, y, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 
 }
