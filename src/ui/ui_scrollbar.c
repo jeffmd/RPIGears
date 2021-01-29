f3ef60029863dc4507b2424d434f21c94fea51f3
jeffmd <shelleydoyle@eastlink.ca>
2021-01-29 12:31:55 -0400

    ui_scrollbar: use bottons and slider inside scrollbar

--- src/ui/ui_scrollbar.c
+++ src/ui/ui_scrollbar.c
@@ -13,12 +13,18 @@
 #include "ui_area_action.h"
 #include "static_array.h"
 #include "ui_icon.h"
+#include "ui_slider.h"
+#include "ui_button.h"
 
 typedef struct {
   uint8_t active;
-  uint8_t scrolling:1;
-  short scroll_area;
   short area;
+  int widget_handle;
+  int inc_button;
+  int dec_button;
+  int slider;
+  float normal_scale[2];
+  float normal_offset[2];
   float select_scale[2];
   float select_offset[2];
 } UI_Scrollbar;
@@ -49,7 +55,6 @@ static UI_Scrollbar *get_ui_scrollbar(short id)
 
 static void ui_scrollbar_init(UI_Scrollbar *ui_scrollbar)
 {
-
 }
 
 static short get_ui_scrollbar_class(void)
@@ -69,10 +74,16 @@ static void update_dimensions(UI_Scrollbar *ui_scrollbar, const short area_id)
 
     ui_scrollbar->area = area_id;
     UI_area_size(area_id, size);
-    ui_scrollbar->select_scale[0] = 2.0f * size[0] - 2.0f;
-    ui_scrollbar->select_scale[1] = 2.0f * size[1] - 2.0f;
-    ui_scrollbar->select_offset[0] = -size[0];
-    ui_scrollbar->select_offset[1] = -size[1];
+
+    ui_scrollbar->normal_scale[0] = 2.0f * size[0] - 2.0f;
+    ui_scrollbar->normal_scale[1] = 2.0f * size[1] - 2.0f;
+    ui_scrollbar->normal_offset[0] = -size[0];
+    ui_scrollbar->normal_offset[1] = -size[1];
+
+    ui_scrollbar->select_scale[0] = 2.0f * size[0] - 6.0f;
+    ui_scrollbar->select_scale[1] = 2.0f * size[1] - 6.0f;
+    ui_scrollbar->select_offset[0] = -size[0] + 2.0f;
+    ui_scrollbar->select_offset[1] = -size[1] + 2.0f;
   }
 }
 
@@ -81,10 +92,11 @@ static void ui_scrollbar_draw(const short area_id, const short ui_scrollbar_id)
   UI_Scrollbar *const ui_scrollbar = get_ui_scrollbar(ui_scrollbar_id);
 
   update_dimensions(ui_scrollbar, area_id);
-  UI_icon_draw_box(ui_scrollbar->select_scale[0], ui_scrollbar->select_scale[1], ui_scrollbar->select_offset[0], ui_scrollbar->select_offset[1]);
+  UI_icon_draw_box(ui_scrollbar->normal_scale[0], ui_scrollbar->normal_scale[1], ui_scrollbar->normal_offset[0], ui_scrollbar->normal_offset[1]);
 
   if (UI_area_is_active(area_id)) {
     UI_icon_draw_box(ui_scrollbar->select_scale[0], ui_scrollbar->select_scale[1], ui_scrollbar->select_offset[0], ui_scrollbar->select_offset[1]);
+
   }
 }
 
@@ -113,16 +125,6 @@ static void ui_scrollbar_area_resize(const short area_id, const short ui_scrollb
 
 static void ui_scrollbar_start_drag(const short area_id, const short ui_scrollbar_id)
 {
-  UI_area_drag_start();
-  UI_area_set_handled(area_id);
-  UI_area_set_locked(area_id);
-}
-
-static void ui_scrollbar_end_drag(const short area_id, const short ui_scrollbar_id)
-{
-  UI_area_drag_end();
-  UI_area_set_handled(area_id);
-  UI_area_set_unlocked(area_id);
 }
 
 static short get_ui_scrollbar_key_map(void)
@@ -130,7 +132,6 @@ static short get_ui_scrollbar_key_map(void)
   if (!ui_scrollbar_key_map) {
     ui_scrollbar_key_map = Key_Map_create();
     Key_Map_add(ui_scrollbar_key_map, Key_Action_create(LEFT_BUTTON, ui_scrollbar_start_drag, 0));
-    Key_Map_add(ui_scrollbar_key_map, Key_Action_create(LEFT_BUTTON_RELEASE, ui_scrollbar_end_drag, 0));
   }
 
   return ui_scrollbar_key_map;
@@ -139,15 +140,6 @@ static short get_ui_scrollbar_key_map(void)
 static void ui_scrollbar_area_key_change(const short area_id, const short ui_scrollbar_id)
 {
   Key_Map_action(get_ui_scrollbar_key_map(), UI_area_active_key(), area_id, ui_scrollbar_id);
-}
-
-static void ui_scrollbar_area_pointer_drag(const short area_id, const short ui_number_id)
-{
-  const short delta_y = UI_area_drag_delta_y();
-
-  if (delta_y != 0) {
-    UI_area_change_offset(area_id, 0, -delta_y);
-  }
 }
 
 static short get_area_connector(void)
@@ -159,7 +151,6 @@ static short get_area_connector(void)
     UI_area_connect_resize(area_connector, ui_scrollbar_area_resize);
     UI_area_connect_attach(area_connector, ui_scrollbar_area_attach);
     UI_area_connect_key_change(area_connector, ui_scrollbar_area_key_change);
-    UI_area_connect_pointer_drag(area_connector, ui_scrollbar_area_pointer_drag);
   }
 
   return area_connector;
@@ -170,14 +161,19 @@ static int get_ui_scrollbar_area_handle(const short ui_scrollbar_id)
   return Connector_handle(get_area_connector(), ui_scrollbar_id);
 }
 
-int UI_scrollbar_create(const short scroll_area)
+void UI_scrollbar_connect_widget(const short ui_scrollbar_id, const int handle)
+{
+  UI_Scrollbar *const ui_scrollbar = get_ui_scrollbar(ui_scrollbar_id);
+  ui_scrollbar->widget_handle = handle;
+}
+
+int UI_scrollbar_create(const int handle)
 {
   const short id = find_deleted_ui_scrollbar();
   UI_Scrollbar *const ui_scrollbar = get_ui_scrollbar(id);
   ui_scrollbar->active = 1;
   ui_scrollbar_init(ui_scrollbar);
-
-  ui_scrollbar->scroll_area = scroll_area;
+  UI_scrollbar_connect_widget(id, handle);
 
   return get_ui_scrollbar_area_handle(id);
 }
