83e5dd2c458aaad7cf96d70f8ee8b1281a591ad9
jeffmd <shelleydoyle@eastlink.ca>
2021-05-11 10:30:35 -0300

    ui_scrollbar: fix wrong parameter type and namein UI_scrollbar_create function - add missing UI_scrollbar_connect_widget function

--- include/ui/ui_scrollbar.h
+++ include/ui/ui_scrollbar.h
@@ -3,6 +3,7 @@
 #ifndef _UI_SCROLLBAR_H_
   #define _UI_SCROLLBAR_H_
 
-  Plug_t UI_scrollbar_create(const ID_t scroll_area);
+  void UI_scrollbar_connect_widget(const ID_t ui_scrollbar_id, const Plug_t widget_plug);
+  Plug_t UI_scrollbar_create(const Plug_t widget_plug);
 
 #endif
